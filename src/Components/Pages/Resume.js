e4599635ca66f593b6a1f54179d516fc2a5cfe55
Brian Gomez <briandgomez98@gmail.com>
2023-06-24 16:12:32 -0500

    attached resume pdf

--- src/Components/Pages/Resume.js
+++ src/Components/Pages/Resume.js
@@ -1,4 +1,19 @@
+import React from "react";
+import { Document, Page, pdfjs } from "react-pdf";
+import BGResume from "../../Brian-Gomez-Resume.pdf";
+import "./Resume.css";
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  "pdfjs-dist/build/pdf.worker.min.js",
+  import.meta.url
+).toString();
+
 function Resume() {
-  return <h1>Resume Page!</h1>;
+  return (
+    <div className="resume-container">
+      <Document file={BGResume}>
+        <Page scale={1} pageNumber={1} />
+      </Document>
+    </div>
+  );
 }
 export default Resume;
