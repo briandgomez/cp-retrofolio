c2d6f382af7e1efb83ee3206f8b1be53457c9be4
Brian Gomez <briandgomez98@gmail.com>
2023-07-10 23:41:22 -0500

    created grid for projects section

--- src/Components/Pages/Projects.js
+++ src/Components/Pages/Projects.js
@@ -1,4 +1,46 @@
+import "./Projects.css";
 function Projects() {
-  return <h1>Projects Page!</h1>;
+  const images = [
+    {
+      src: require("../../Images/YER.jpg"),
+      title: "Y.E.R.",
+      text: "Sample text",
+    },
+    {
+      src: require("../../Images/Team-Generator.png"),
+      title: "Team",
+      text: "Sample text",
+    },
+    {
+      src: require("../../Images/Social.png"),
+      title: "Social API",
+      text: "Sample text",
+    },
+    {
+      src: require("../../Images/E-Commerce.png"),
+      title: "E-Commerce",
+      text: "Sample text",
+    },
+    {
+      src: require("../../Images/Employee.png"),
+      title: "Employee",
+      text: "Sample text",
+    },
+  ];
+
+  return (
+    <div class="container">
+      <div class="row row-cols-3">
+        {images.map((image) => (
+          <img
+            key={image}
+            src={image.src}
+            height={"235px"}
+            style={{ padding: "15px" }}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 export default Projects;
