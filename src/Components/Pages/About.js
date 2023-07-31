2246b01cf65e79265c7117cdbbb404c25a44a2d1
Brian Gomez <briandgomez98@gmail.com>
2023-07-30 21:51:47 -0500

    added glitch and typewriter effect to about page

--- src/Components/Pages/About.js
+++ src/Components/Pages/About.js
@@ -1,4 +1,35 @@
+import { useGlitch } from "react-powerglitch";
+import { TypeAnimation } from "react-type-animation";
+import "./About.css";
+
 function About() {
-  return <h1>About Page!</h1>;
+  const glitch = useGlitch();
+  return (
+    <div>
+      <div>
+        <div>
+          {" "}
+          <span id="glitch-text" ref={glitch.ref}>
+            Welcome
+          </span>{" "}
+        </div>
+        <TypeAnimation
+          wrapper="span"
+          style={{
+            fontSize: "2em",
+            whiteSpace: "pre-line",
+            height: "195px",
+            display: "inline-block",
+          }}
+          speed={50}
+          sequence={[
+            `Hi,\nMy name is Brian Gomez. I am a Application Developer who enjoys working on challenging projects. I have experience in front and back end development. In my free time I enjoy staying active, watching films/tv-shows/anime, or exploring the world`,
+            1000,
+          ]}
+          repeat={Infinity}
+        />
+      </div>
+    </div>
+  );
 }
 export default About;
