89a57232ea3c8bec836f51418257dbc46cb9d2d3
Brian Gomez <briandgomez98@gmail.com>
2023-08-02 00:11:38 -0500

    changed size and hover effect for sidebar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -30,17 +30,11 @@ function Navbar() {
         </div>
         <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
           <ul className="nav-menu-items" onClick={showSidebar}>
-            <li className="navbar-toggle">
-              <Link to="#" className="menu-bars">
-                <AiIcons.AiOutlineClose></AiIcons.AiOutlineClose>
-              </Link>
-            </li>
             {SideBarData.map((item, index) => {
               return (
                 <li key={index} className={item.cName}>
                   <Link to={item.path}>
                     {item.icon}
-                    <span>{item.title}</span>
                   </Link>
                 </li>
               );
