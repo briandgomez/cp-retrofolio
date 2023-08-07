53cb46851ab89f5f7342456262664e72049d8dbd
Brian Gomez <briandgomez98@gmail.com>
2023-08-07 18:32:43 -0500

    removed server code

--- src/Components/Pages/Contact.js
+++ src/Components/Pages/Contact.js
@@ -1,31 +1,9 @@
 import "bootstrap/dist/css/bootstrap.css";
-import { useState } from "react";
 import "./Contact.css";
 
 const Contact = () => {
-  const [status, setStatus] = useState("Submit");
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setStatus("Sending...");
-    const { name, email, message } = e.target.elements;
-    let details = {
-      name: name.value,
-      email: email.value,
-      message: message.value,
-    };
-    let response = await fetch("http://localhost:5000/contact", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json;charset=utf-8",
-      },
-      body: JSON.stringify(details),
-    });
-    setStatus("Submit");
-    let result = await response.json();
-    alert(result.status);
-  };
   return (
-    <form id="contact-form" onSubmit={handleSubmit}>
+    <form id="contact-form">
       <h2 id="contact-me-h2">Contact Me</h2>
       <div>
         <label htmlFor="name">Name:</label>
@@ -62,7 +40,7 @@ const Contact = () => {
         className="btn"
         type="submit"
       >
-        {status}
+        Submit
       </button>
     </form>
   );
