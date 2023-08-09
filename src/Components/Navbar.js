ad8e1a3c6c96876acc1bc699754a2df71699da06
Brian Gomez <briandgomez98@gmail.com>
2023-08-08 23:59:22 -0500

    fixed navbar icon positions

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
 import { Link } from "react-router-dom";
 import { SideBarData } from "./SidebarData";
 import "./Navbar.css";
