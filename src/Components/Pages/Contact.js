b6f5c803861e68526dbb99195c86e2dc369db58b
Brian Gomez <briandgomez98@gmail.com>
2023-08-11 10:04:23 -0500

    endabled form submission

---
+++ src/Components/Pages/Contact.js
@@ -0,0 +1,51 @@
+import "bootstrap/dist/css/bootstrap.css";
+import { useState } from "react";
+
+const Contact = () => {
+  return (
+    <form name="contact" id="contact-form" data-netlify="true">
+      <h2 id="contact-me-h2">Contact Me</h2>
+      <div>
+        <label htmlFor="name">Name:</label>
+        <input
+          className="form-control"
+          name="name-input"
+          type="text"
+          id="name"
+          placeholder="Full Name"
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          name="email-input"
+          className="form-control"
+          id="email"
+          required
+          aria-describedby="emailHelp"
+          placeholder="Your Email Address"
+        />
+      </div>
+      <div>
+        <label htmlFor="message">Message:</label>
+        <textarea className="form-control" id="message" required />
+      </div>
+      <button
+        id="submit-button"
+        style={{
+          margin: "6px",
+          borderColor: "#05d9e8",
+          color: "white",
+        }}
+        className="btn"
+        type="submit"
+      >
+        Submit
+      </button>
+    </form>
+  );
+};
+
+export default Contact;
