acd55b7e56dfaf87ea3efb17b4db0e4a2d79f352
Brian Gomez <briandgomez98@gmail.com>
2023-08-13 18:00:44 -0500

    created custom success page when submitting form

---
+++ src/Components/Pages/Succcess.js
@@ -0,0 +1,16 @@
+import "./Success.css";
+const Succcess = () => {
+  return (
+    <div class="content">
+      <div class="fa fa-check-circle-o symbol"></div>
+      <div class="title">
+        Your for has been submitted!
+        <br />
+        I'll get back to you as soon as I can.
+      </div>
+      <div class="text"></div>
+    </div>
+  );
+};
+
+export default Succcess;
